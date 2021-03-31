fccc0a45c30ed2b835ed291ecdcff41ddd08cdd5
AlandaAquino <alandaoaquino@fac.pe.senac.br>
2021-03-31 13:12:13 -0300

    criação do get, post, put e delete

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
-const { request } = require('express');
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
 
 // console.log(app);
+/*
+GET: buscar
+*/
 
-app.get('/', (request, response) => {
-    response.send('Olá, Mundo!');
+app.get('/projects', (request, response) => {
+    // response.send('Olá, Mundo!');
+    const {title, owner} = request.query;
+
+    console.log(title);
+    console.log(owner);
+
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ])
+})
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+        'Projeto 5',
+        'Projeto 6',
+    ])
+})
+
+app.put('/projects/:id', (request, response) => {
+    const params = request.params;
+
+    console.log(params)
+    
+    return response.json([
+        'Projeto 50',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+        'Projeto 5',
+        'Projeto 6',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 50',
+        'Projeto 2',
+    ])
 })
 
 app.listen(3000, () => {
