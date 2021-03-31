e5202cf8cfb9604bf35d661e8af8d395b50ecd3f
AlandaAquino <alandaoaquino@fac.pe.senac.br>
2021-03-31 14:44:01 -0300

    Update

--- index.js
+++ index.js
@@ -1,41 +1,28 @@
 const { request, response } = require('express');
 const express = require('express');
+const {v4:uuidv4} = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
-// console.log(app);
-/*
-GET: buscar
-*/
+const projects = [];
+
 
 app.get('/projects', (request, response) => {
-    // response.send('Olá, Mundo!');
-    const {title, owner} = request.query;
+    //const {title, owner} = request.query;
 
-    console.log(title);
-    console.log(owner);
-
-    return response.json([
-        'Projeto 1',
-        'Projeto 2'
-    ])
+    return response.json(projects)
 });
 
 app.post('/projects', (request, response) => {
-    const body = request.body;
+    const {title, owner} = request.body;
 
-    console.log(body);
+    const project = {id: uuidv4(), title, owner};
+
+    projects.push(project); // Esse push vai jogar a criação do nosso projeto para o nosso array
     
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4',
-        'Projeto 5',
-        'Projeto 6',
-    ])
+    return response.json(project); // sempre retornar o projeto recém criado e nunca exibir a lista completa
 });
 
 app.put('/projects/:id', (request, response) => {
